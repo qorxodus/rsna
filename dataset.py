acbe508e16590fdf20110705fcf9d155bb9a8319
EC2 Default User <ec2-user@ip-172-31-27-136.us-east-2.compute.internal>
2023-08-30 01:57:25 +0000

    Adapting pipeline to new dataset

--- dataset.py
+++ dataset.py
@@ -9,22 +9,26 @@ from torch.utils.data import Dataset, DataLoader
 class RSNADataset(Dataset):
     def __init__(self, dataframe, image_directory, transform = None):
         super().__init__()
-        self.image_ids = dataframe['patientId'].unique()
+        self.image_ids = dataframe['patientId'] #.unique()
         self.dataframe = dataframe
         self.image_directory = image_directory
         self.transform = transform
         
     def __getitem__(self, index):
-        image_id = self.image_ids[index]
-        info = self.dataframe[self.dataframe['patientId'] == image_id]
-        image = cv2.imread(f'{self.image_directory}/{image_id}.png', cv2.IMREAD_COLOR)
+
+        # I need to make it so that it doesn't only check for when the patient id equals the image id. I need to go 
+        # one by one to make sure that they are all separate. Otherwise, they're gonna be a 2d info df
+        # image_id = self.image_ids[index]
+        # info = self.dataframe[self.dataframe['patientId'] == image_id]
+        info = self.dataframe.loc[index]
+        image = cv2.imread(f'{self.image_directory}/{info[0]}.png', cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32) / 255.0
-        boxes, labels = info[['x', 'y', 'width', 'height']].values, info['Target'].values
-        boxes[:, 2] += boxes[:, 0]
-        boxes[:, 3] += boxes[:, 1]
+        boxes, labels = info[1:5], info[5]
+        boxes[2] += boxes[0]
+        boxes[3] += boxes[1]
         sample = self.transform(**{'image': image, 'boxes': boxes})
-        target = {'boxes': torch.from_numpy(sample['boxes'].astype(np.float32)), 'labels': torch.from_numpy(labels.astype(np.int64))}
-        return sample['image'], target, image_id
+        target = {'boxes': torch.tensor(sample['boxes'].astype(np.float32)), 'labels': torch.tensor(labels.astype(np.int64))}
+        return sample['image'], target, info[0]
 
     def __len__(self):
         return self.image_ids.shape[0]
@@ -41,19 +45,19 @@ def collate(batch):
 def prepare_data():
     directory = f"/home/ec2-user/rsna/train_images_png"
     dataframe = pd.read_csv(f"/home/ec2-user/rsna/stage_2_train_labels.csv")
-    dataframe_positive = pd.DataFrame(columns = ['patientId', 'x', 'y', 'width', 'height', 'Target'])
-    k = 0
-    for i in range(len(dataframe)):
-        if dataframe.loc[i]['Target'] == 1:
-            dataframe_positive.loc[k] = dataframe.loc[i]
-            k += 1
-    image_ids = dataframe['patientId'].unique()
+    # dataframe_positive = pd.DataFrame(columns = ['patientId', 'x', 'y', 'width', 'height', 'Target'])
+    # k = 0
+    # for i in range(len(dataframe)):
+    #     if dataframe.loc[i]['Target'] == 1:
+    #         dataframe_positive.loc[k] = dataframe.loc[i]
+    #         k += 1
+    image_ids = dataframe['patientId'] #.unique()
     train_ids = image_ids[:-5000]
     valid_ids = image_ids[-5000:-2500]
     test_ids = image_ids[-2500:]
-    train_dataframe = dataframe_positive[dataframe_positive['patientId'].isin(train_ids)]
-    valid_dataframe = dataframe_positive[dataframe_positive['patientId'].isin(valid_ids)]
-    test_dataframe = dataframe_positive[dataframe_positive['patientId'].isin(test_ids)]
+    train_dataframe = dataframe[dataframe['patientId'].isin(train_ids)]
+    valid_dataframe = dataframe[dataframe['patientId'].isin(valid_ids)]
+    test_dataframe = dataframe[dataframe['patientId'].isin(test_ids)]
     train_dataset = RSNADataset(train_dataframe, directory, get_train_transform())
     valid_dataset = RSNADataset(valid_dataframe, directory, get_valid_test_transform())
     test_dataset = RSNADataset(test_dataframe, directory, get_valid_test_transform())
--- pipeline_CNN.py
+++ pipeline_CNN.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import numpy as np
 from tqdm import tqdm
 
 def train(dataloader, model, optimizer, device, loss_history, loss_function_class, loss_function_box):
@@ -8,12 +9,15 @@ def train(dataloader, model, optimizer, device, loss_history, loss_function_clas
     loss_history.reset()
     progress_bar = tqdm(total = len(dataloader))
     for i, (images, targets, _) in enumerate(dataloader):
-        images = list(image.to(device) for image in images)
+        images = torch.stack([image.to(device) for image in images])
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         predicted = model(images)
         predicted_label = torch.sigmoid(predicted[:, 0])
         predicted_box = predicted[:, 1:5]
-        loss_class = loss_function_class(predicted_label.float(), targets['labels'].tolist())
-        loss_box = loss_function_box(predicted_box, targets['boxes'].tolist())
+        labels = torch.Tensor([target['labels'] for target in targets]).to(device)
+        boxes = torch.stack([target['boxes'] for target in targets]).to(device)
+        loss_class = loss_function_class(predicted_label.float(), labels)
+        loss_box = loss_function_box(predicted_box, boxes)
         loss = loss_class + 0.00001 * loss_box
         loss_history.send(loss.item())
         optimizer.zero_grad()
