2221a0afc4ae5dafba7d704ea4951d7da42c19a7
EC2 Default User <ec2-user@ip-172-31-27-136.us-east-2.compute.internal>
2023-09-17 21:08:40 +0000

    Fix targets formatting issue in FRCNN pipeline.

--- pipeline_FRCNN.py
+++ pipeline_FRCNN.py
@@ -1,5 +1,6 @@
 import time
 from tqdm import tqdm
+import torch
 
 def train(dataloader, learning_rate_scheduler, model, optimizer, device, loss_history, epoch):
     model.train()
@@ -10,7 +11,15 @@ def train(dataloader, learning_rate_scheduler, model, optimizer, device, loss_hi
         batch_start = time.time()
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        print(targets.size)
+
+        # Convert target format to be compatible with FRCNN
+        for i in range(len(targets)):
+            targets[i]['boxes'] = targets[i]['boxes'].unsqueeze(0)
+            targets[i]['labels'] = targets[i]['labels'].unsqueeze(0)
+
+        # TODO(@Taeyeon): filter out bbox for negative samples
+
+
         loss_dictionary = model(images, targets)
         losses = sum(loss for loss in loss_dictionary.values())
         loss_value = losses.item()
@@ -20,6 +29,6 @@ def train(dataloader, learning_rate_scheduler, model, optimizer, device, loss_hi
         optimizer.step()
         progress_bar.update()
         batch_end = time.time()
-        print(f"Epoch #{epoch}, Loss: {loss.item():.4f}, Time: {(batch_end - batch_start) / 60:.3f} Minutes", end = '\r')
+        print(f"Epoch #{epoch}, Loss: {losses.item():.4f}, Time: {(batch_end - batch_start) / 60:.3f} Minutes", end = '\r')
     end = time.time()
     return loss_history, end, start
